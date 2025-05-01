977157ca3e02fc6bb0294690369ff5393febbcf5
Brian Blosser <bpb54321@gmail.com>
2025-05-01 09:15:20 -0400

    Store count in an variable external to the component function to ensure that the alert always shows the actual number of times the user clicked.

--- src/Components/ClosureComponent/index.js
+++ src/Components/ClosureComponent/index.js
@@ -1,18 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+let globalCount
+
+const INITIAL_COUNT = 0
 
 function ClosureComponent() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(INITIAL_COUNT)
+
+  useEffect(() => {
+    globalCount = INITIAL_COUNT
+  }, [])
 
   function handleAlertClick() {
     setTimeout(() => {
-      alert(`You clicked ${count} times`)
+      alert(`You clicked ${globalCount} times`)
     }, 2500)
+  }
+
+  function handleIncrementClick() {
+    const incrementedCount = count + 1
+    globalCount = incrementedCount
+    setCount(incrementedCount)
   }
 
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button type="button" onClick={() => setCount(count + 1)}>
+      <button type="button" onClick={handleIncrementClick}>
         Click me
       </button>
       <button type="button" onClick={handleAlertClick}>
