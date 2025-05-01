34b867caa81ade3242d2652842fdf15ff21b41a1
Brian Blosser <bpb54321@gmail.com>
2025-05-01 09:46:25 -0400

    Extract PersonForm component add allow toggling between display and edit mode.

--- src/Components/PersonListForm/index.js
+++ src/Components/PersonListForm/index.js
@@ -1,7 +1,41 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
 import faker from 'faker'
 import { nanoid } from 'nanoid'
+
+function PersonForm({ field }) {
+  const [isEditing, setIsEditing] = useState(false)
+  return (
+    <li>
+      {isEditing ? (
+        <form
+          onSubmit={event => {
+            event.preventDefault()
+            setIsEditing(false)
+          }}
+        >
+          <input type="text" />
+          <button type="submit">Update</button>
+        </form>
+      ) : (
+        <div>
+          <span>{field.name}</span>
+          <button type="button" onClick={() => setIsEditing(true)}>
+            Edit
+          </button>
+        </div>
+      )}
+    </li>
+  )
+}
+
+PersonForm.propTypes = {
+  field: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+}
 
 function PersonListForm() {
   const [fields, setFields] = useState([
@@ -22,10 +56,7 @@ function PersonListForm() {
       </button>
       <ol>
         {fields.map((field, index) => (
-          <li key={index}>
-            {field.name}:<br />
-            {/* <input type="text" /> */}
-          </li>
+          <PersonForm field={field} key={index} />
         ))}
       </ol>
     </>
